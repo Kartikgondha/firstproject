d90a64e69f7ecb763231046e63a28596eca9d57f
Kartikgondha <kartikgondha2000@gmail.com>
2022-05-27 12:30:21 +0530

    26_5

---
+++ src/Component/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                hello react
+            </div>
+        );
+    }
+}
+
+export default Demo;
