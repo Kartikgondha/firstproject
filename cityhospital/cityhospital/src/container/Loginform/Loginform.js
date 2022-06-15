3b64915e7f753fdb2e13e01b5b3d6370ba1fbc28
Kartikgondha <kartikgondha2000@gmail.com>
2022-06-15 09:53:26 +0530

    cityhospital

--- cityhospital/cityhospital/src/container/Loginform/Loginform.js
+++ cityhospital/cityhospital/src/container/Loginform/Loginform.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import "./Loginform.css";
 
 
@@ -11,86 +11,72 @@ function Loginform(props) {
     const [password, setPassword] = useState(false);
 
     return (
-<div className='background'>
-<section id="appointment" className="appointment">
-            <div className="container">
-                <div className="section-title">
-                    {
-                        useType === 'Login' && password === false ? 
-                        <h2>Login</h2>
-                         : password === true ?
-                         <h2>Forgot password</h2>:
-                          <h2>Signup</h2>
-                    }
-                
-                    <div action method="post" role="form" className="php-email-form">
-                      {
-                          useType === 'Login' ? 
-                         <>
-                          <div className="row">
-                          <div className="col-md-4 form-group mt-3 mt-md-0">
-                              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                              <div className="validate" />
-                          </div>
-                      </div>
-                      <div className="row">
-                          <div className="col-md-4 form-group mt-3 mt-md-0">
-                              <input type="password" className="form-control" name="password" id="password" placeholder="Your password" data-rule="password" data-msg="Please enter a valid email" />
-                              <div className="validate" />
-                          </div>
-                      </div>
-                         </>
-                         :
-                        <>
-                         <div className="row">
-                                <div className="col-md-4 form-group">
-                                <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                <div className="validate" />
+        <div className='background'>
+            <section id="appointment" className="appointment">
+                <div className="container">
+                    <div className="section-title">
+                        {
+                            useType === 'Login' && password === false ?
+                                <h2>Login</h2>
+                                : password === true ?
+                                    <h2>Forgot password</h2> :
+                                    <h2>Signup</h2>
+                        }
+
+                        <div action method="post" role="form" className="php-email-form">
+                            {
+                                useType === 'Signup' ?
+
+                                    <div className="row">
+                                        <div className="col-md-4 form-group">
+                                            <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                            <div className="validate" />
+                                        </div>
+                                    </div> :null
+                            }
+                            <div className="row">
+                                <div className="col-md-4 form-group mt-3 mt-md-0">
+                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                    <div className="validate" />
+                                </div>
+                            </div>
+                            {
+                                password === true ? null :
+                            <div className="row">
+                                <div className="col-md-4 form-group mt-3 mt-md-0">
+                                    <input type="password" className="form-control" name="password" id="password" placeholder="Your password" data-rule="password" data-msg="Please enter a valid email" />
+                                    <div className="validate" />
+                                </div>
+                            </div>
+                            }
+
+                            {
+                                useType === 'Login' && password === false ?
+                                    <>
+                                        <a onClick={() => setuseType('Signup')} className="linkcolor">Creat Have An Account ?</a><br/>
+                                        <a onClick={() => setPassword(true)} className="linkcolor">Forgot password ?</a></>
+                                    :password === true ?
+                                        <a onClick={() => setPassword(false)} className="linkcolor">Remember Your Password ?</a> :
+                                        <a onClick={() => setuseType('Login')} className="linkcolor">Already Have An Account ?</a>
+
+                            }
+
+
+
+
+                            <div className='row'>
+                                {
+                                    useType === 'Login' ?
+                                        <div className="text-center"><button type="submit">Login</button></div>
+                                        :
+                                        <div className="text-center"><button type="submit">Signup</button></div>
+                                }
                             </div>
                         </div>
-                        
-                        <div className="row">
-                        <div className="col-md-4 form-group mt-3 mt-md-0">
-                            <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                            <div className="validate" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4 form-group mt-3 mt-md-0">
-                            <input type="password" className="form-control" name="password" id="password" placeholder="Your password" data-rule="password" data-msg="Please enter a valid email" />
-                            <div className="validate" />
-                        </div>
-                    </div>
-                       </>
-
-                        
-                      } 
-                        {
-                           useType === 'Login' ? 
-                          <>
-                           <a onClick={()=>setuseType('Signup')} className="linkcolor">Creat Have An Account ?</a></>
-                           :
-                           <>
-                            <a onClick={()=>setuseType('Login')} className="linkcolor">Already Have An Account ?</a></>
-
-                        } 
-
-                      
-                       
-
-                    <div className='row'>
-                      {
-                          useType === 'Login' ?
-                          <div className="text-center"><button type="submit">Login</button></div>
-                          :
-                          <div className="text-center"><button type="submit">Signup</button></div>
-                      }
-                    </div>
                     </div>
                 </div>
-            </div>
-        </section>
-</div>
+            </section>
+        </div>
 
 
     );
